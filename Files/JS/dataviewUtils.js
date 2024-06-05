e4fe1a78586789543b84288a1c3606a9e78005ed
Ruslan Seit-Akaev <r.seit-akaev@dunice.net>
2024-06-05 17:41:51 +0300

    vault backup: 2024-06-05 17:41:51

--- Files/JS/dataviewUtils.js
+++ Files/JS/dataviewUtils.js
@@ -2,13 +2,13 @@ const getTaskLabelByItLevel = (dv, level) => {
   const element = dv.el("span", level, {
     cls: `task task_${level}`,
   });
-  
+
   return element;
-}
+};
 
 const getPathFromRootToTasksFolder = () => {
   return "Base/Road-Map/DSA/Tasks";
-}
+};
 
 const utils = {
   getTaskLabelByItLevel,
